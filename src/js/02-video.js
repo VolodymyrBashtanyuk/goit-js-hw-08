0e2c52dfa37a30d648b76154c56157cc7446462b
VolodymyrBashtanyuk <vladimirbashtanuk@gmail.com>
2022-07-12 14:47:39 +0300

    task-02 complete

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
 
+import Player from '@vimeo/player';
+import Throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe);
+player.on('timeupdate', Throttle(playingTime, 1000));
+
+function playingTime  ({ seconds }) {
+    localStorage.setItem('timeupdate', seconds);
+};
+player.setCurrentTime(localStorage.getItem('timeupdate'));
