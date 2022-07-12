f48da76b7f7404726bc706a28654f65c2d273156
VolodymyrBashtanyuk <vladimirbashtanuk@gmail.com>
2022-07-12 10:06:09 +0300

    update

---
+++ src/js/01-gallery.js
@@ -0,0 +1,30 @@
+// Add imports above this line
+
+import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+// Change code below this line
+
+// console.log(SimpleLightbox);
+
+const galleryItemList = document.querySelector('.gallery');
+const galleryList = createGalleryList(galleryItems);
+galleryItemList.insertAdjacentHTML('beforeend', galleryList)
+
+
+function createGalleryList(itemList) {
+    return itemList.map(({ preview, original, description }) => {
+        return `<a class="gallery__link" href="${original}">
+    <img
+      loading = 'lazy'
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+    </a>`;
+    }).join(' ');
+};
+
+new SimpleLightbox( '.gallery a', {captionSelector: 'img', captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250 });
+
+
